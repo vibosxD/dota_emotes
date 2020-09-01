cea7c26232d12a9ff5ee17df3089595cb639cf67
vibos <69803008+vibosxD@users.noreply.github.com>
2020-09-01 17:25:29 -0400

    some stuff

--- server.js
+++ server.js
@@ -5,14 +5,15 @@ import fs from 'fs'
 import util from 'util'
 const readFile = util.promisify(fs.readFile)
 
-const app = express()
+const app = express()  
+
 app.get('/', async (req, res) => {
   const index = (await readFile('./index.html')).toString()
   res.send(index)
 })
 app.use('/emoticons', express.static('emoticons'))
 app.get('/emoticons.json', async (req, res) => {
-  const emoteJson = (await readFile('./emoticons.json')).toString()
+  const emoteJson = JSON.parse((await readFile('./emoticons.json')).toString())
   res.json(emoteJson)
 })
 app.listen(3024, () => {})
