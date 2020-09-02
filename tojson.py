95eefee7d097d5750f209fb9eb8e09bb253068bd
vibos <69803008+vibosxD@users.noreply.github.com>
2020-09-02 01:35:36 -0400

    nothin

--- tojson.py
+++ tojson.py
@@ -2,10 +2,10 @@
 import re
 with open('emoticons.txt', 'r') as f:
   data = re.sub(r'\s*//.*\n', '\n', f.read()) # Remove Comments
-  data = re.sub(r'"[{ \t]*"', '": "', data) # Add key: value
-  data = re.sub(r'\"\n\s*\{', '":{', data) # Add key: object
-  data = re.sub(r'\}\n*\s*"', '},"', data) # Add comma after key: {object}
-  data = re.sub(r'"(\n+|[ \t]*\n+)[ \t]*"', '","', data) # Add comma between key: value
+  data = re.sub(r'"[{ \t]*"', '": "', data) # Add ':' to key: value
+  data = re.sub(r'\"\n\s*\{', '":{', data) # Add '{' to key: {object}
+  data = re.sub(r'\}\n*\s*"', '},"', data) # Add ',' after key: {object}
+  data = re.sub(r'"(\n+|[ \t]*\n+)[ \t]*"', '","', data) # Add ',' after key: value
   data = "{" + data + "}"
   with open('emoticons.json', 'w') as f:
     f.write(data)
